d82eec96c0f7d4727af9524470c7ae34eac80eec
leoalvarado <calvaradot1@est.ups.edu.ec>
2023-03-03 14:09:06 -0500

    New dialog for client search. New field for the total value of a bill.

--- src/main/java/microdev/vaadinspringcomercial/view/FacturacionView.java
+++ src/main/java/microdev/vaadinspringcomercial/view/FacturacionView.java
@@ -1,6 +1,5 @@
 package microdev.vaadinspringcomercial.view;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -9,12 +8,14 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
-import com.vaadin.flow.data.binder.Binder;
 import microdev.vaadinspringcomercial.model.ClientModel;
+import microdev.vaadinspringcomercial.model.ProdTem;
+import microdev.vaadinspringcomercial.repository.ProdTemRepository;
 
 import java.util.Objects;
 
@@ -22,8 +23,10 @@ import java.util.Objects;
 public class FacturacionView extends Div {
 
     private ClientModel selectedClient;
+    private ProdTemRepository prodTemRepository;
 
-    public FacturacionView() {
+    public FacturacionView(ProdTemRepository prodTemRepository) {
+        this.prodTemRepository = prodTemRepository;
         selectedClient = new ClientModel();
         addTopComponent();
     }
@@ -32,30 +35,60 @@ public class FacturacionView extends Div {
         Button addClientDataButton = new Button("Buscar Cliente");
         addClientDataButton.addClickListener(e -> addClientDataButtonEvent());
 
-        NumberField totalTxtArea = new NumberField();
-        totalTxtArea.getStyle().set("color", "#DC143C");
-        totalTxtArea.getStyle().set("font-size", "150%");
-        totalTxtArea.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
-        totalTxtArea.setPrefixComponent(new Label("$"));
+        NumberField totalNumberField = new NumberField();
+        totalNumberField.getStyle().set("color", "#DC143C");
+        totalNumberField.getStyle().set("font-size", "150%");
+        totalNumberField.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        totalNumberField.setPrefixComponent(new Label("$"));
         // TODO: Bring the value from a controller package class.
-        totalTxtArea.setValue(123.321);
+        totalNumberField.setValue(123.321);
+
+
 
         FormLayout formLayout = new FormLayout();
-        formLayout.add(addClientDataButton, totalTxtArea);
+        formLayout.add(addClientDataButton, totalNumberField);
 
-        formLayout.setResponsiveSteps(
-                new FormLayout.ResponsiveStep("0", 1),
+        formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
                 new FormLayout.ResponsiveStep("500px", 2));
 
         this.add(formLayout);
     }
-
 
     private void addClientDataButtonEvent() {
         // TODO Refactor this method to make it shorter!
         Dialog clientSearchDialog = new Dialog();
         clientSearchDialog.setHeaderTitle("Buscar un cliente");
 
+        TextField direccionTextField = new TextField("Direccion");
+        TextField telefonoTextField = new TextField("Telefono");
+        EmailField emailEmailField = new EmailField("Correo:");
+        emailEmailField.setErrorMessage("Correo invalido");
+
+        Button enviarConsumidorFinalButton = new Button("Consumidor Final");
+        Button enviarClienteButton = new Button("Seleccionar Cliente");
+        enviarClienteButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        enviarClienteButton.addClickListener(e ->
+                enviarClienteAction(direccionTextField.getValue(), telefonoTextField.getValue(), emailEmailField,
+                        clientSearchDialog));
+        enviarConsumidorFinalButton.addClickListener(e -> {
+            enviarConsumidorFinalAction();
+            clientSearchDialog.close();
+        });
+
+        FormLayout datosClienteFormLayout = new FormLayout();
+
+        ComboBox<ClientModel>[] comboBoxes = this.generateComboboxForDialog(emailEmailField, direccionTextField, telefonoTextField);
+
+        datosClienteFormLayout.add(comboBoxes[0], comboBoxes[1], direccionTextField, telefonoTextField,
+                emailEmailField, enviarClienteButton, enviarConsumidorFinalButton);
+
+        clientSearchDialog.add(datosClienteFormLayout);
+        clientSearchDialog.open();
+    }
+
+    private ComboBox<ClientModel>[] generateComboboxForDialog(EmailField emailEmailField, TextField direccionTextField,
+                                                              TextField telefonoTextField) {
         ComboBox<ClientModel> cedulaClienteComboBox = new ComboBox<>("Cedula/RUC:");
         // TODO: Remove dummy data for real data
         cedulaClienteComboBox.setItems(ClientModel.getDummyData());
@@ -64,7 +97,6 @@ public class FacturacionView extends Div {
         cedulaClienteComboBox.setAllowCustomValue(true);
 
         ComboBox<ClientModel> nombreClienteComboBox = new ComboBox<>("Nombre:");
-        //Custom Filter for lowercase name searching
         ComboBox.ItemFilter<ClientModel> nombreItemFilter = (client, filterString)
                 -> client.getNombre().toLowerCase().contains(filterString.toLowerCase());
         // TODO: Dummy data
@@ -72,11 +104,6 @@ public class FacturacionView extends Div {
         nombreClienteComboBox.setItemLabelGenerator(ClientModel::getNombre);
         nombreClienteComboBox.setPlaceholder("Ingrese el nombre del cliente");
         nombreClienteComboBox.setAllowCustomValue(true);
-
-        TextField direccionTextField = new TextField("Direccion");
-        TextField telefonoTextField = new TextField("Telefono");
-        EmailField emailEmailField = new EmailField("Correo:");
-        emailEmailField.setErrorMessage("Correo invalido");
 
         cedulaClienteComboBox.addValueChangeListener(e -> {
             this.selectedClient = e.getValue();
@@ -87,57 +114,43 @@ public class FacturacionView extends Div {
             this.selectedClient = e.getValue();
             this.showClientData(cedulaClienteComboBox, emailEmailField, direccionTextField, telefonoTextField);
         });
-        // Listener for custom values
         nombreClienteComboBox.addCustomValueSetListener(e -> this.selectedClient.setNombre(e.getDetail()));
 
-        Button enviarClienteButton = new Button("Seleccionar Cliente");
-        enviarClienteButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        enviarClienteButton.addClickListener(e -> enviarClienteAction(direccionTextField.getValue(),
-                telefonoTextField.getValue(), emailEmailField));
-
-        Button enviarConsumidorFinalButton = new Button("Consumidor Final");
-        enviarConsumidorFinalButton.addClickListener(e -> enviarConsumidorFinalAction());
-
-        FormLayout datosClienteFormLayout = new FormLayout();
-
-
-
-        datosClienteFormLayout.add(cedulaClienteComboBox, nombreClienteComboBox, direccionTextField, telefonoTextField,
-                emailEmailField, enviarClienteButton, enviarConsumidorFinalButton);
-
-        // Add form layout to dialog
-        clientSearchDialog.add(datosClienteFormLayout);
-
-        clientSearchDialog.open();
+        return new ComboBox[]{cedulaClienteComboBox, nombreClienteComboBox};
     }
 
-    private void showClientData(ComboBox<ClientModel> comboBox, EmailField emailField, TextField... textFields){
+    private void showClientData(ComboBox<ClientModel> comboBox, EmailField emailField, TextField... textFields) {
         comboBox.setValue(this.selectedClient);
         emailField.setValue(this.selectedClient.getEmail());
         textFields[0].setValue(this.selectedClient.getDireccion());
         textFields[1].setValue(this.selectedClient.getTelefono());
     }
 
-    private void enviarClienteAction(String direccion, String telefono, EmailField email) {
-        if(Objects.nonNull(this.selectedClient.getId()) && Objects.nonNull(this.selectedClient.getNombre())){
-            if(!direccion.isBlank() && !telefono.isBlank() && (!email.isInvalid() && !email.getValue().isBlank())){
+    private void enviarClienteAction(String direccion, String telefono, EmailField email, Dialog parent) {
+        if (Objects.nonNull(this.selectedClient.getId()) && Objects.nonNull(this.selectedClient.getNombre())) {
+            if (!direccion.isBlank() && !telefono.isBlank() && (!email.isInvalid() && !email.getValue().isBlank())) {
                 this.selectedClient.setDireccion(direccion);
                 this.selectedClient.setTelefono(telefono);
                 this.selectedClient.setEmail(email.getValue());
-                System.out.println(selectedClient);
-            }else{
-                Notification.show("Revise que los campos direccion, telefono o email esten correctamente ingresados",
-                        5000, Notification.Position.TOP_CENTER);
+                this.showNotification("Se ha cargado el cliente con el nombre: " + this.selectedClient.getNombre(), false);
+                parent.close();
+            } else {
+                this.showNotification("Revise que los campos direccion, telefono o email esten correctamente ingresados", true);
             }
-        }else{
-            Notification.show("Ingrese el RUC y el Nombre correctamente",
-                    5000, Notification.Position.TOP_CENTER);
+        } else {
+            this.showNotification("Ingrese el RUC y el Nombre correctamente", true);
         }
     }
 
+    private void showNotification(String text, boolean isError) {
+        Notification notification = new Notification(text,
+                5000, Notification.Position.TOP_CENTER);
+        notification.addThemeVariants(isError ? NotificationVariant.LUMO_ERROR : NotificationVariant.LUMO_SUCCESS);
+        notification.open();
+    }
+
     private void enviarConsumidorFinalAction() {
-        Notification.show("Se generara la factura como consumidor final!", 5000, Notification.Position.TOP_CENTER);
+        this.showNotification("Consumidor Final!", false);
     }
 
 }
