8ad1d532fe862674b0bb2f83453df1d585ee60d7
leoalvarado <calvaradot1@est.ups.edu.ec>
2023-03-03 20:36:27 -0500

    Hashcode and equals. Relationship altered.

--- src/main/java/microdev/vaadinspringcomercial/model/ProductModel.java
+++ src/main/java/microdev/vaadinspringcomercial/model/ProductModel.java
@@ -1,8 +1,14 @@
 package microdev.vaadinspringcomercial.model;
 
 import lombok.*;
-import javax.persistence.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -20,9 +26,21 @@ public class ProductModel {
     private int cantidadInterna;
     private BigDecimal valorPaquete;
     private BigDecimal descuento;
-    @OneToOne(mappedBy = "productModel")
+    @ManyToOne
     private TaxModel impuesto;
     @ManyToOne
     private ProviderModel providerModel;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ProductModel that = (ProductModel) o;
+        return getCodPrincipal() != null && Objects.equals(getCodPrincipal(), that.getCodPrincipal());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
